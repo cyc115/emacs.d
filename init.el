101a605a06ce6d70bc2210324e555ef63af2ce60
Chen <code@getbraintree.com>
2018-10-26 15:41:26 -0500

    moved .spacemacs file into .emacs.d, new emacs setups need to link `ln -s .emacs.d/.spacemacs ~/.spacemacs`
    added ruby autoformater: mike/rubocopfmt.el
    added a timer: mike/timer.el

--- init.el
+++ init.el
@@ -32,3 +32,75 @@
   (spacemacs/setup-startup-hook)
   (require 'server)
   (unless (server-running-p) (server-start)))
+
+
+;; load-user-file
+;; load a custom .el lib from ~/.emacs.d/ directory
+(defconst user-init-dir
+  (cond ((boundp 'user-emacs-directory)
+         user-emacs-directory)
+        ((boundp 'user-init-directory)
+         user-init-directory)
+        (t "~/.emacs.d/")))
+
+
+(defun load-user-file (file)
+  (interactive "f")
+  "Load a file in current user's configuration directory"
+  (load-file (expand-file-name file user-init-dir)))
+
+;; custom setups
+(set-default 'truncate-line t)
+(set-default 'word-wrap t)
+(desktop-save-mode 1)
+
+;; enable mouse scrolling in osx
+(unless window-system
+  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+  (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
+
+
+;; org-mode setups
+(setq org-todo-keywords
+      '((sequence "IDEA" "TODO" "WIP" "PAUSED" "|" "DONE" "CANCELLED" "NOTE"  )))
+
+(setq org-todo-keyword-faces
+      '(
+        ("IDEA" . (:foreground "pink" :weight bold))
+        ("WIP" . (:foreground "blue" :weight bold))
+        ("CANCELLED" . (:foreground "green" :weight bold))
+        ("PAUSED" . (:foreground "yellow" :weight bold))
+        ))
+
+(setq org-startup-indented t)
+
+;; org-mode archive-done-tasks
+(defun my-org-archive-done-tasks ()
+  (interactive)
+  (org-map-entries 'org-archive-subtree "/DONE" 'file)
+  (org-map-entries 'org-archive-subtree "/CANCELLED" 'file)
+  )
+
+
+;; projectile mode setup
+(projectile-mode +1)
+
+;; minapp development setups
+(setq auto-mode-alist (append '(("\\.wpy$" . vue))
+                              auto-mode-alist))
+
+;; switch window mode
+(setq switch-window-shortcut-style 'qwerty)
+(setq switch-window-qwerty-shortcuts
+      '("a" "s" "d" "f" "j" "k" "l" ";" "w" "e" "i" "o"))
+(setq switch-window-shortcut-appearance 'asciiart)
+
+;; load timer
+(load-user-file "mike/timer.el")
+
+;; load ruby robocop fmter
+(load-user-file "mike/robocopfmt.el")
+(add-hook 'ruby-mode-hook #'rubocopfmt-mode)
+
+;;; accomodate dropbox
+(setq desktop-base-file-name (concat ".desktop." (system-name)))

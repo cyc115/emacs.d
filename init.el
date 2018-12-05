6f88b3cff01b5f94ddaea77846e5d9c326d4fc83
Chen <code@getbraintree.com>
2018-12-05 14:48:00 -0600

    Allow eww mode to open multiple buffers by default

--- init.el
+++ init.el
@@ -134,6 +134,24 @@ buffer in current window."
 ;; display time inn the status line
 (display-time-mode 1)
 
+;; eww browser open links in separate buffer by default
+;; Auto-rename new eww buffers, so when running `M-x eww` a new buffer is created
+(defun xah-rename-eww-hook ()
+  "Rename eww browser's buffer so sites open in new page."
+  (rename-buffer "eww" t))
+(add-hook 'eww-mode-hook #'xah-rename-eww-hook)
+
+;; C-u M-x eww will force a new eww buffer
+(defun modi/force-new-eww-buffer (orig-fun &rest args)
+  "When prefix argument is used, a new eww buffer will be created,
+regardless of whether the current buffer is in `eww-mode'."
+  (if current-prefix-arg
+      (with-temp-buffer
+        (apply orig-fun args))
+    (apply orig-fun args)))
+(advice-add 'eww :around #'modi/force-new-eww-buffer)
+
+
 ;; open links from eamcs in the emacs embedded w3m browser
 ;; (setq browse-url-browser-function  'w3m-goto-url-new-session)
 
@@ -192,6 +210,8 @@ buffer in current window."
 (setq org-agenda-cmp-user-defined 'my/user-todo-sort)
 (setq org-agenda-cmp-user-defined 'my/user-todo-sort)
 
+;; display inline image size
+(setq org-image-actual-width nil)
 
 
 ;; ------------------------- language specific sections below ---------------------------------------

4f8d9073bdd728d2b5a916237f012bd975f7b1ac
Mike Chen <mike.chen@getbraintree.com>
2019-01-26 05:22:29 +0000

    remove version check

--- init.el
+++ init.el
@@ -23,19 +23,14 @@
 (defconst spacemacs-version         "0.200.13" "Spacemacs version.")
 (defconst spacemacs-emacs-min-version   "24.4" "Minimal version of Emacs.")
 
-(if (not (version<= spacemacs-emacs-min-version emacs-version))
-    (error (concat "Your version of Emacs (%s) is too old. "
-                   "Spacemacs requires Emacs version %s or above.")
-           emacs-version spacemacs-emacs-min-version)
-  (load-file (concat (file-name-directory load-file-name)
-                     "core/core-load-paths.el"))
-  (require 'core-spacemacs)
-  (spacemacs/init)
-  (configuration-layer/sync)
-  (spacemacs-buffer/display-startup-note)
-  (spacemacs/setup-startup-hook)
-  (require 'server)
-  (unless (server-running-p) (server-start)))
+(load-file (concat (file-name-directory load-file-name) "core/core-load-paths.el"))
+(require 'core-spacemacs)
+(spacemacs/init)
+(configuration-layer/sync)
+(spacemacs-buffer/display-startup-note)
+(spacemacs/setup-startup-hook)
+(require 'server)
+(unless (server-running-p) (server-start))
 
 ;; ------------------------- tools / libraries / custom setups ------------------------------------------
 

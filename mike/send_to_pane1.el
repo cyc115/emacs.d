8b31a5a48469ad80bddd4c214d27f30a657e681a
Mike Chen <yuechuan20@gmail.com>
2019-06-10 20:53:52 -0500

    Add elisp silent-command to execute shell cmd silently

--- mike/send_to_pane1.el
+++ mike/send_to_pane1.el
@@ -1,32 +1,37 @@
 ;; here we use C-c b to send cmd to pane 1 and C-c w to send to pane 2
 
+(defun silent-command (cmd)
+  (call-process-shell-command
+   cmd
+   nil "*Shell Command Output*" t
+   ))
 
 (defun my/tmux-swap-pane-1-with-0 ()
   (interactive)
-  (shell-command "tmux swap-pane -s0 -t1")
-  (shell-command "tmux select-pane -t2")
+  (silent-command "tmux swap-pane -s0 -t1")
+  (silent-command "tmux select-pane -t2")
   )
 
 (defun my/tmux-send-to-pane-1 ()
   (interactive)
-  (shell-command
+  (silent-command
    (s-concat "tmux send-keys -t 1 '" (thing-at-point 'line t) "'" )))
 
 (defun my/tmux-send-C-c-to-pane-1 ()
   (interactive)
-  (shell-command
+  (silent-command
    "tmux send-keys -t 1 C-c"
    ))
 
 (defun my/tmux-send-C-d-to-pane-1 ()
   (interactive)
-  (shell-command
+  (silent-command
    "tmux send-keys -t 1 C-d"
    ))
 
 (defun my/tmux-send-to-pane-2 ()
   (interactive)
-  (shell-command
+  (silent-command
    (s-concat "tmux send-keys -t 2 '" (thing-at-point 'line t) "'" ))
   )
 
@@ -47,7 +52,7 @@
         (setq pos1 (car bds) pos2 (cdr bds))))
 
     ;;(message "Region: %s" (buffer-substring pos1 pos2) )
-    (shell-command
+    (silent-command
      (s-concat "tmux send-keys -t 1 '" (buffer-substring pos1 pos2) "'" ))
     ))
 
@@ -63,7 +68,7 @@
         (setq pos1 (car bds) pos2 (cdr bds))))
 
     ;;(message "Region: %s" (buffer-substring pos1 pos2) )
-    (shell-command
+    (silent-command
      (s-concat "tmux send-keys -t 2 '" (buffer-substring pos1 pos2) "'" ))
     ))
 

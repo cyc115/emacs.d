4682a4e44d1be601d3d852975e301d88232e090b
Mike Chen <yuechuan20@gmail.com>
2020-05-17 16:23:29 -0500

    Remove clock report from Agenda (it takes too much room and I never look at it); Add section that shows unscheduled :thisweek: entries.

--- mike/custom-org-functions.el
+++ mike/custom-org-functions.el
@@ -56,12 +56,16 @@
           (agenda "" nil)
           ;; show additional lists
           (tags-todo ":thisweek:work:" ((org-agenda-overriding-header "This week @ work")))
-          (tags-todo "thisweek-work" ((org-agenda-overriding-header "This week")))
+          (tags-todo "thisweek-work" ((org-agenda-overriding-header "This week -work and not scheduled")
+                                      (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled))))
+          (tags-todo "thisweek" ((org-agenda-overriding-header "This week (all)")))
           (tags-todo "Q1" ((org-agenda-overriding-header "This Quarter")))
-          (tags-todo "inbox" ((org-agenda-overriding-header "All inbox"))))
+          (tags-todo "inbox" ((org-agenda-overriding-header "All inbox")))
+          )
 
-         ((org-agenda-start-with-log-mode '(closed clock state))
-          (org-agenda-start-with-clockreport-mode t)
+         (
+          (org-agenda-start-with-log-mode '(closed clock state))
+          ;; (org-agenda-start-with-clockreport-mode t)
           )
          )
         ("p" "Agenda and personal related tasks"
